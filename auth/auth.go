82284443be7790e8c603f0e8814a769c0efb0c32
cermu <kwendosamson750@gmail.com>
2021-04-13 23:42:01 +0300

    updated CreateToken function to cater for refresh token too

--- auth/auth.go
+++ auth/auth.go
@@ -3,23 +3,57 @@ package auth
 import (
 	utl "github.com/cermu/Go-phoneBook-API/utils"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/twinj/uuid"
 	"time"
 )
 
-// CreateToken public function that returns a JWT auth token
-func CreateToken(accountId uint) (string, error) {
-	jwtSecret := utl.ReadConfigs().GetString("JWT.ACCESS_SECRET") // fetch secret key
+// AuthenticationDetails struct to store token claims
+type AuthenticationDetails struct {
+	AccessToken        string
+	RefreshToken       string
+	AccessUuid         string
+	RefreshUuid        string
+	AccessTokenExpire  int64
+	RefreshTokenExpire int64
+}
 
+// CreateToken public function that returns a JWT auth token
+func CreateToken(accountId uint) (*AuthenticationDetails, error) {
+	var err error
+	authDetails := &AuthenticationDetails{}
+	jwtAccessSecret := utl.ReadConfigs().GetString("JWT.ACCESS_SECRET")
+	jwtRefreshSecret := utl.ReadConfigs().GetString("JWT.REFRESH_SECRET")
+
+	// access token valid for 15 minutes only
+	authDetails.AccessTokenExpire = time.Now().Add(time.Minute * 15).Unix()
+	authDetails.AccessUuid = uuid.NewV4().String()
+
+	// refresh token valid for 7 days only
+	authDetails.RefreshTokenExpire = time.Now().Add(time.Hour * 24 * 7).Unix()
+	authDetails.RefreshUuid = uuid.NewV4().String()
+
+	// creating access token
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["account_id"] = accountId
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix() // token valid for 15 mins only
-
+	atClaims["access_uuid"] = authDetails.AccessUuid
+	atClaims["exp"] = authDetails.AccessTokenExpire
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err := at.SignedString([]byte(jwtSecret))
+	authDetails.AccessToken, err = at.SignedString([]byte(jwtAccessSecret))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return token, nil
+	// creating refresh token
+	rtClaims := jwt.MapClaims{}
+	rtClaims["account_id"] = accountId
+	rtClaims["refresh_uuid"] = authDetails.RefreshUuid
+	rtClaims["exp"] = authDetails.RefreshTokenExpire
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	authDetails.RefreshToken, err = rt.SignedString([]byte(jwtRefreshSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	return authDetails, nil
 }
